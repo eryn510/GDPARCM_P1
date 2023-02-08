136e6a15219736d4e3c3951757d820e2080ffd29
eryn510 <eryn_gabriel_tallador@dlsu.edu.ph>
2023-02-08 19:58:30 +0800

    copy paste template

---
+++ GDPARCM_P1/GDPARCM_P1/main.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <stdio.h>
+#include "HelloWorldThread.h"
+#include "IETThread.h"
+#include <thread>
+
+void createHWThreads()
+{
+	for (int i = 0; i < 20; i++)
+	{
+		HelloWorldThread* thread = new HelloWorldThread(i);
+		thread->start();
+	}
+
+	//Sleep main thread to avoid premature halting. Wait for other threads to finish execution.
+	IETThread::sleep(1000);
+}
+
+void testFunctionThread()
+{
+	std::cout << "Hello world from a thread. " << std::endl;
+}
+
+int main() {
+	//createHWThreads();
+	std::thread myThread(testFunctionThread);
+	myThread.join();
+}
