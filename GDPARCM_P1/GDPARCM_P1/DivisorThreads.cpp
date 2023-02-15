bcbe03c6003ccb6a79ddfad4c11ce6fb2545878e
eryn510 <eryn_gabriel_tallador@dlsu.edu.ph>
2023-02-15 22:29:46 +0800

    code cleanup

--- GDPARCM_P1/GDPARCM_P1/DivisorThreads.cpp
+++ GDPARCM_P1/GDPARCM_P1/DivisorThreads.cpp
@@ -16,33 +16,19 @@ void DivisorThreads::run()
 {
 	int minDivisor = ((dividend / 2) / maxThreads) * (threadCount); 
 	int maxDivisor = ((dividend / 2) / maxThreads) * (threadCount + 1);
-
-
-
-
-	//std::cout << "maxdivisorcheck:" + std::to_string(maxDivisor) << std::endl;
-
-
-
 	
 	for (int i = minDivisor; i <= maxDivisor; i++)
 	{
 		if (i == 0)
 			i = 2;
 
-		//std::cout << "Checking: " << i << std::endl;
 
 		if (dividend % i == 0)
 		{
-			//std::cout << "Not a Prime Number" << std::endl;
-			//std::cout << "Divisor: " << i << std::endl;
 			this->isNumberPrime = false;
 			this->returnFlag = true;
 			break;
 		}
-		
-		//IETThread::sleep(1000);
-		
 	}
 
 	this->returnFlag = true;
--- GDPARCM_P1/GDPARCM_P1/main.cpp
+++ GDPARCM_P1/GDPARCM_P1/main.cpp
@@ -11,8 +11,6 @@
 void createDivisorThreads(int testNumber, int maxThreads, int threadCount, std::vector<DivisorThreads*>* threadList)
 {
 	
-
-
 	for (int i = 0; i < maxThreads; i++)
 	{
 		DivisorThreads* thread = new DivisorThreads(testNumber, maxThreads, threadCount);
@@ -21,8 +19,6 @@ void createDivisorThreads(int testNumber, int maxThreads, int threadCount, std::
 		threadCount += 1;
 		
 	}
-
-	//IETThread::sleep(1000);
 }
 
 int main()
@@ -31,7 +27,7 @@ int main()
 
 	int TEST_INT = 2147483647;
 	int curr_threadCount = 0;
-	int maxThreads = pow(2, 2);
+	int maxThreads = pow(2, 10);
 	int isNumberPrime = true;
 
 	bool isAllThreadsDone = false;
@@ -40,7 +36,6 @@ int main()
 
 	createDivisorThreads(TEST_INT, maxThreads, curr_threadCount, &threadList);
 
-	//IETThread::sleep(5000);
 
 	while (isAllThreadsDone == false) 
 	{
@@ -54,11 +49,6 @@ int main()
 		}
 	}
 
-
-	/*
-	 *TO DO: print only once lang if number is prime or nah
-	 *
-	 */
 	for(int i = 0; i < threadList.size(); i++)
 	{
 		if (threadList[i]->isNumberPrime == false)
